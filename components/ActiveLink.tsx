409bdeb87c721f2dac90022842cd7ba900ea7876
aleramosAR <aleramos.ok@gmail.com>
2023-01-13 17:43:39 -0300

    pre-deployment-vercel

---
+++ components/ActiveLink.tsx
@@ -0,0 +1,25 @@
+import { FC, CSSProperties } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { UrlObject } from 'url';
+
+const style:CSSProperties = {
+  color: '#0070f3',
+  textDecoration: 'underline'
+}
+
+interface Props {
+  text: string;
+  href: string;
+}
+
+export const ActiveLink:FC<Props> = ({ text, href }) => {
+
+  const { asPath } = useRouter()
+
+  return (
+    <Link href={href}>
+      <span style={ asPath == href ? style : undefined }>{text}</span>
+    </Link>
+  )
+}
